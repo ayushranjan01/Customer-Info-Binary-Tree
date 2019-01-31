899572471b5a3482e4470763ed015576c9cb519b
Ayush Ranjan <35203726+ayushranjan01@users.noreply.github.com>
2019-01-31 11:59:38 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		TheBinaryTree customerTree = new TheBinaryTree();
+		CustomerInfo someCustomer;
+		someCustomer = new CustomerInfo(00, "Jason", "Bourne");
+		customerTree.addToTree(customerTree.getrootnode(), someCustomer);
+
+//Creating a new binary tree and a new customerInfo object and adding the customerInfo to the binary tree
+
+	}
+}
